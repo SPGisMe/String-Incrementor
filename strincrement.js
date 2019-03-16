537f8dda3e58ffd035ee38d7902be6b21cf035db
Sean Gaston <46758834+SPGisMe@users.noreply.github.com>
2019-03-16 16:26:31 -0700

    Update strincrement.js

--- strincrement.js
+++ strincrement.js
@@ -1,8 +1,9 @@
 function incrementString (strng) {
   let result = '';
+  let incrementor = 1;
   let nums = strng.match(/\d+/g);
   let str = strng.match(/\D+/g);
-  let numPlus = Number(nums) + 1;
+  let numPlus = Number(nums) + incrementor;
   
   str === null ? result = numPlus.toString().padStart(nums[0].length, '0')
   : nums === null ? result = str.join('') + '1'
